cb56f8f9e759d2fce88612b6a00041f5510d4a01
Sofia Arciniegas <sofiarciniegas17@gmail.com>
2018-08-28 17:19:10 -0500

    orioin

---
+++ app/src/main/java/com/example/worldskills/aplicationprueba/Objeto.java
@@ -0,0 +1,23 @@
+package com.example.worldskills.aplicationprueba;
+
+public class Objeto {
+
+
+    private int edad;
+
+
+    public Objeto(int edad) {
+        this.edad = edad;
+    }
+
+
+    /// ALEX HOLIS C:
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+}
